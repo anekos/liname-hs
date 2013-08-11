f3482b095ba73b9686ff39a867332f714edba671
anekos <anekos@snca.net>
2013-08-11 15:29:34 +0900

    Add cabal files

--- src/liname.hs
+++ src/liname.hs
@@ -2,6 +2,8 @@
 import           LiName.Types
 import           LiName.Parsers
 
+import           Control.Applicative
+import           Control.Monad
 import           Text.Parsec.Prim (runP)
 import           Text.Parsec.Error (ParseError)
 
@@ -10,3 +12,5 @@ import           Text.Parsec.Error (ParseError)
 test :: String -> Either ParseError LinameEntry
 test s = runP entryParser () "<TEST>" s
 
+main = do ls <- lines <$> getContents
+          forM_ ls (print . test)
