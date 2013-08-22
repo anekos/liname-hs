5a0790af7f617b2d4d42403b09684db304e0c16a
anekos <anekos@snca.net>
2013-08-22 22:52:55 +0900

    Update main module

--- src/liname.hs
+++ src/liname.hs
@@ -1,23 +1,40 @@
 
-import LiName.Types
-import LiName.Parsers
-import LiName.Operators
-import LiName.Editor
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import LiName.Actions
+import LiName.Command
 import LiName.Loader
+import LiName.Parsers
+import LiName.Types
 
+import Prelude hiding (lookup)
 import Control.Applicative ((<$>), (<*>))
-import Control.Monad (mapM)
+import Control.Lens
+import Control.Monad (forM, forM_)
+import Data.ByteString.UTF8 (fromString, toString)
 import Data.Default (def)
-import Data.Either (rights, lefts)
+import Data.Either (rights)
+import Data.Map (Map, fromList)
+import Data.Map.Lazy (lookup)
 import System.Environment (getArgs)
-import Text.Parsec.Error (ParseError)
-import Text.Parsec.Prim (runP)
+import Text.Printf (printf)
+import Text.ShellEscape (escape)
 
+
+
+sourceLine :: LiNameSource -> String
+sourceLine ((LiNameKey key), path) = printf "%.4d\t%s" key $ toString $ escape $ fromString path
+
+
+process :: LiNameConfig -> Map LiNameKey LiNamePath -> LiNameEntry -> IO (Either String ())
+process conf sm (LiNameEntry {_entryKey = key, _action = act })
+    | Just fp <- lookup key sm = doAction conf act fp
+    | otherwise                = return $ Left $ "Not found key: " ++ show key
 
 
 main = do
-    src <- getArgs
-    es <- map entryLine <$> makeEntries <$> loadPath' src
-    es' <- map (parseEntry "<TEMP>") <$> edit def es
-    print $ rights es'
-    print $ lefts es'
+    let conf = def :: LiNameConfig
+    ss :: [LiNameSource] <- makeSources <$> (loadPath' =<< getArgs)
+    es' <- map (parseEntry "<TEMP>") <$> edit (def^.editorCommand) (map sourceLine ss)
+    results <- forM (rights es') $ process conf (fromList ss)
+    forM_ results print
