39f30c1b3294141241c029d5210243ff3f2824f6
anekos <anekos@snca.net>
2013-08-22 23:39:29 +0900

    Use default if any error occured while loading config file

--- src/LiName/Config.hs
+++ src/LiName/Config.hs
@@ -15,9 +15,20 @@ import Data.Default (def)
 import Data.Yaml.Config
 import System.Directory (getHomeDirectory)
 import System.FilePath (combine)
-import System.IO.Error (catchIOError)
 import Data.Text (pack)
 import Control.Applicative ((<$>))
+
+
+
+loadConfigFile :: IO LiNameConfig
+loadConfigFile = do
+    home <- getHomeDirectory
+    y <- load $ combine home ".liname.yaml"
+    ec <- makeCommand' y "editor" editorCommandDefault
+    tc <- makeCommand' y "trash" trashCommandDefault
+    return $ LiNameConfig { _editorCommand = ec, _trashCommand = tc }
+  `catch`
+    catchAndReturnDefault def
 
 
 makeCommand :: LiNameCommand -> Config -> LiNameCommand
@@ -26,15 +37,8 @@ makeCommand d c = LiNameCommand { _path = lookupDefault c (pack "command") (d^.p
                                 , _placeHolder = lookupDefault c (pack "place_holder") (d^.placeHolder) }
 
 
-loadConfigFile :: IO LiNameConfig
-loadConfigFile = do
-    home <- getHomeDirectory
-    y <- load $ combine home ".liname.yaml"
-    ec <- withDefault editorCommandDefault $ makeCommand editorCommandDefault <$> subconfig y (pack "editor")
-    tc <- withDefault trashCommandDefault $ makeCommand editorCommandDefault <$> subconfig y (pack "trash")
-    return $ LiNameConfig { _editorCommand = ec, _trashCommand = tc }
-  `catchIOError`
-    (return . const def)
+makeCommand' :: Config -> String -> LiNameCommand -> IO LiNameCommand
+makeCommand' y n d = withDefault d $ makeCommand d <$> subconfig y (pack n)
 
 
 withDefault :: a -> IO a -> IO a
