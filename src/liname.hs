a25908f4aa2e74b027c64e822d18ee75a3b52e10
anekos <anekos@snca.net>
2013-08-26 19:21:21 +0900

    Mofu mofu fix

--- src/liname.hs
+++ src/liname.hs
@@ -33,7 +33,7 @@ main' :: Either String (LiNameConfig, [String]) -> IO ()
 main' (Left err)               = hPutStrLn stderr err
 main' (Right (conf, pathArgs)) = do
     ss :: [LiNameSource] <- makeSources <$> loadPath' pathArgs
-    es' <- map (parseEntry "<TEMP>") <$> edit (def^.editorCommand) (map sourceLine ss)
+    es' <- map (parseEntry "<TEMP>") <$> edit (conf^.editorCommand) (map sourceLine ss)
     results <- forM (rights es') $ process conf (fromList ss)
     clean $ rights results
     putResult ss (rights es') results
