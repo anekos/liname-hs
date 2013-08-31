8011592faed6f426e2980a55cab4d5c50cbc8a1f
anekos <anekos@snca.net>
2013-08-31 17:37:51 +0900

    Fix and add tests for Path.hs

--- src/LiName/Path.hs
+++ src/LiName/Path.hs
@@ -10,6 +10,7 @@ import System.FilePath.Posix (joinPath, splitDirectories)
 
 
 commonPath :: [String] -> [String] -> [String]
+commonPath [_] [_] = []
 commonPath (x:xs) (y:ys)
     | x == y      = x : commonPath xs ys
     | otherwise   = []
@@ -23,10 +24,15 @@ commonPath' (x1:x2:xs) = commonPath' $ commonPath x1 x2 : xs
 
 
 compactPath :: Int -> [String] -> (String, [String])
+compactPath _ [x] = let p = splitDirectories x
+                        cut = max 0 $ length p - 1
+                    in  (joinPath $ take cut p, [joinPath $ drop cut p])
 compactPath lv xs = let ps = map splitDirectories xs
+                        shortest = minimum $ map length ps
                         cp = commonPath' ps
-                        cut = drop $ max 0 $ length cp - lv
-                    in  (joinPath cp, map (joinPath . cut) ps)
+                        cpl = length cp
+                        cut = min (shortest - 1) $ max 0 $ cpl  - lv
+                    in  (joinPath $ take cut cp, map (joinPath . drop cut) ps)
 
 
 compactPath' :: Maybe Int -> [String] -> (String, [String])
---
+++ test/LiName/PathSpec.hs
@@ -0,0 +1,36 @@
+
+module LiName.PathSpec where
+
+
+import LiName.Types
+import LiName.Path
+
+import Data.Either.Unwrap
+import Test.Hspec
+import Text.ParserCombinators.Parsec.Error
+
+
+spec :: Spec
+spec = do
+    describe "compactPath'" $ do
+      it "Nothing" $ do
+        compactPath' Nothing ["/a/b/X", "/a/b/Y"] `shouldBe` ("", ["/a/b/X", "/a/b/Y"])
+        compactPath' Nothing ["/Q/b/X", "/a/b/Y"] `shouldBe` ("", ["/Q/b/X", "/a/b/Y"])
+      it "Multi files" $ do
+        compactPath' (Just 0) ["/a/b/X", "/a/b/Y"] `shouldBe` ("/a/b", ["X", "Y"])
+        compactPath' (Just 0) ["/Q/b/X", "/a/b/Y"] `shouldBe` ("/", ["Q/b/X", "a/b/Y"])
+      it "Level Specified" $ do
+        compactPath' (Just 1) ["/a/b/X", "/a/b/Y"] `shouldBe` ("/a", ["b/X", "b/Y"])
+        compactPath' (Just 2) ["/a/b/X", "/a/b/Y"] `shouldBe` ("/", ["a/b/X", "a/b/Y"])
+        compactPath' (Just 3) ["/a/b/X", "/a/b/Y"] `shouldBe` ("", ["/a/b/X", "/a/b/Y"])
+        compactPath' (Just 4) ["/a/b/X", "/a/b/Y"] `shouldBe` ("", ["/a/b/X", "/a/b/Y"])
+        compactPath' (Just 5) ["/a/b/X", "/a/b/Y"] `shouldBe` ("", ["/a/b/X", "/a/b/Y"])
+      it "Single file" $ do
+        compactPath' (Just 0) ["/a/b/X"] `shouldBe` ("/a/b", ["X"])
+        compactPath' (Just 0) ["/a"] `shouldBe` ("/", ["a"])
+        compactPath' (Just 0) ["a"] `shouldBe` ("", ["a"])
+      it "Same files" $ do
+        let x = "/a/b/X"
+        compactPath' (Just 0) [x, x, x] `shouldBe` ("/a/b", ["X", "X", "X"])
+        compactPath' (Just 1) [x, x, x] `shouldBe` ("/a", ["b/X", "b/X", "b/X"])
+        compactPath' (Just 2) [x, x, x] `shouldBe` ("/", ["a/b/X", "a/b/X", "a/b/X"])
