d648b6911e96105a089837c49caa34e9c6f6ed62
anekos <anekos@snca.net>
2013-11-15 23:56:37 +0900

    WIP

--- src/LiName/Config.hs
+++ src/LiName/Config.hs
@@ -17,6 +17,9 @@ import Data.Yaml.Config
 import System.Directory (getHomeDirectory)
 import System.FilePath (combine)
 import Data.Text (pack)
+import Text.JSON
+import Text.JSON.Types
+import Text.JSON.Generic
 import Control.Applicative ((<$>))
 
 
@@ -24,17 +27,35 @@ import Control.Applicative ((<$>))
 loadConfigFile :: IO LiNameConfig
 loadConfigFile = do
     home <- getHomeDirectory
-    y <- load $ combine home ".liname.yaml"
-    ec <- makeCommand' y "editor" editorCommandDefault
-    tc <- makeCommand' y "trash" trashCommandDefault
-    return $ def { _editorCommand = ec
-                 , _trashCommand = tc
-                 , _compact = lookup y (pack "compact")
-                 , _sortType = maybe DontSort readSortType $ lookup y (pack "sort")
-                 , _ignoreName = maybe (def^.ignoreName) Just $ lookup y (pack "ignore-name")
-                 , _ignorePath = maybe (def^.ignorePath) Just $ lookup y (pack "ignore-path") }
-  `catch`
-    catchAndReturnDefault def
+    let fn = combine home ".liname.json"
+    jsonFile <- readFile fn
+    let root = decode jsonFile :: Result (JSObject JSValue)
+
+    let fs = [ entry recursive toBool id True $ root >>= valFromObj "recursive"
+             , entry compact toInt Just Nothing $ root >>= valFromObj "compact"
+             ] :: [LiNameConfig -> LiNameConfig]
+
+    return $ foldl (flip id) def fs
+
+
+entry mut typef conv d json = set mut $ withDef typef conv d json
+
+
+withDef :: (a -> JSValue -> a) -> (a -> b) -> b -> Result JSValue -> b
+withDef f conv d v = case resultToEither v of
+                       (Right v) -> conv $ f d v
+                       (Left v)  -> d
+
+
+toInt :: Int -> JSValue -> Int
+toInt _ (JSRational _ v) = ceiling v
+toInt d _                = d
+
+
+toBool :: Bool -> JSValue -> Bool
+toBool _ (JSRational _ 1) = True
+toBool _ (JSRational _ _) = False
+toBool d _                = d
 
 
 makeCommand :: LiNameCommand -> Config -> LiNameCommand
