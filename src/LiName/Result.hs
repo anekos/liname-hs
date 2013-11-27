a25e126bb1243609a2319ab74dfa628fc2f33d51
anekos <anekos@snca.net>
2013-11-27 23:31:46 +0900

    Fix result indent

--- src/LiName/Result.hs
+++ src/LiName/Result.hs
@@ -13,13 +13,13 @@ import Text.Printf (printf)
 
 
 putResult :: String -> Int -> [LiNameResult] -> IO ()
-putResult src targes rs = do
+putResult src target rs = do
     let fails = lefts rs
     putStrLn "Results"
-    putStrLn $ printf (indent "%5s:   %4d") src targes
-    putStrLn $ printf (indent "try:      %4d") $ length rs
-    putStrLn $ printf (indent "success:  %4d") $ length $ rights rs
-    putStrLn $ printf (indent "fail:     %4d") $ length fails
+    putStrLn $ printf (indent "%-7s      %4d") (src ++ ":") target
+    putStrLn $ printf (indent "try:         %4d") $ length rs
+    putStrLn $ printf (indent "success:     %4d") $ length $ rights rs
+    putStrLn $ printf (indent "fail:        %4d") $ length fails
     unless (null fails) $ do
       hPutStrLn stderr "Fails"
       forM_ fails $ \(line, err) -> hPutStrLn stderr $ indent line ++ "\n" ++ indent ('\t' : unbreak err)
